86e22c9d7bb30986368eca474f9541e600c64c70
ubuntu <jean.queval1@gmail.com>
2016-10-26 17:56:42 +0200

    copie fonctions scala

--- scala/BoschFunction.scala
+++ scala/BoschFunction.scala
@@ -1,9 +1,54 @@
-object fun {
 
-	def mapSplitString(s: String): Array[String] = { s.split(",", -1) }
 
-	def mapTo_FEATURES_id_value(line: Array[String])
-  	{
-		var allValues:Array[String] = new Array[String](nbFeatures_broadcast.value)
+def mapSplitString(s: String): Array[String] = { s.split(",", -1) }
+
+
+
+
+// def mapTo_FEATURES_id_value(line: Array[String], nbFeatures: org.apache.spark.broadcast.Broadcast[Int], listFeatures: org.apache.spark.broadcast.Broadcast[Array[String]]): Array[Tuple2[String, Tuple2[String, String]]] = {
+// 	var lineReturn:Array[Tuple2[String, Tuple2[String, String]]] = new Array[Tuple2[String, Tuple2[String, String]]](nbFeatures.value)
+// 	for (i <- 1 to nbFeatures.value) {
+// 		lineReturn(i-1) = (listFeatures.value(i), (line(0), line(i)))
+
+// 	}
+// 	lineReturn
+// }
+
+def mapTo_FEATURES_id_value(line: Array[String], nbFeatures: Int, listFeatures: Array[String]): Array[Tuple2[String, Tuple2[String, String]]] = {
+	var lineReturn = new Array[Tuple2[String, Tuple2[String, String]]](nbFeatures-1)
+	for (i <- 1 to nbFeatures-1) {
+		lineReturn(i-1) = (listFeatures(i), (line(0), line(i)))
+
+	}
+	lineReturn
+}
+
+def initializeConcatenate(feature_value: (String, String)) = {
+	if (feature_value._2 == "")
+		"" 
+	else 
+		feature_value._1
+}
+
+def combinerConcatenate (pattern: String, feature_value: (String, String)) = {
+	if (feature_value._2 == "") {
+		pattern
+	} 
+	else {
+		if (pattern.length() > 0 ){
+			pattern + ", "  + feature_value._1
+		}
+		else {
+			feature_value._1
+		}
+	}
+}
+
+def mergeConcatenate(pattern1: String, pattern2: String) = {
+	if ((pattern1 != "") & (pattern2 != "")){
+		pattern1 + ", " + pattern2
+	}
+	else {
+		pattern1 + pattern2
 	}
 }
--- scala/comparaisonRDDdataFrame.scala
+++ scala/comparaisonRDDdataFrame.scala
@@ -5,22 +5,72 @@ import org.apache.spark.sql.functions._
 
 import spark.implicits._
 
-:load BoschFunction.scala
+val spark = SparkSession.builder().appName("Spark SQL basic example").config("spark.some.config.option", "some-value").getOrCreate()
 
-val RDDinitiales = sc.textFile("/train_categorical50.csv", 32)
+
+:load BoschFunction.scala
+:load packages/myUDF
+
+
+val RDDinitiales = sc.textFile("/train_categorical300.csv", 64)
 val list_features0 = RDDinitiales.take(1)
 
 val withoutFirstLine = RDDinitiales.filter(line => line != list_features0(0))
 
 
-val list_features = fun.mapSplitString(list_features0(0))
+val list_features = mapSplitString(list_features0(0))
 
 val nbFeatures = list_features.length
 
 val list_features_broadcast = sc.broadcast(list_features)
 val nbFeatures_broadcast = sc.broadcast(nbFeatures)
 
-val RDD1 = withoutFirstLine.map(fun.mapSplitString)
+
+val RDD1 = withoutFirstLine.map(mapSplitString)
 
 
-val RDD_features_FEATURES_value = RDD1.map(mapTo_FEATURES_id_value)
+val RDD_features_FEATURES_value = RDD1.map(line => mapTo_FEATURES_id_value(line, nbFeatures_broadcast.value, list_features_broadcast.value))
+
+val RDD_all_values_FEATURE_id_value = RDD_features_FEATURES_value.flatMap(element => element).cache()
+
+//Création du dataFrames
+
+val RDD_feature_id_value = RDD_all_values_FEATURE_id_value.map(feature_id_values => (feature_id_values._1, feature_id_values._2._1, feature_id_values._2._2)).persist()
+
+
+case class X(feature: String, Id: String, value: String)
+
+val DF_three_columns = RDD_feature_id_value.map (feature_id_values => X(feature_id_values._1, feature_id_values._2, feature_id_values._3)).toDF().persist()
+
+//2: Le nombre d’occurence de chaque variable ((‘’: ), (‘T1: 50000 fois”), …)
+
+//RDD:
+val RDD_VALUE_id_feature = RDD_feature_id_value.map(FEATURE_id_value => (FEATURE_id_value._3, (FEATURE_id_value._2, FEATURE_id_value._1)))
+
+val RDD_countOccurenciVaraible = RDD_VALUE_id_feature.combineByKey((value: (String, String)) => 1, (conteur: Int, newValue: (String, String)) => conteur + 1, (conteur1:Int, conteur2:Int) => conteur1 + conteur2)
+
+//DF:
+val DF_countOccurenciVaraible = DF_three_columns.groupBy("value").count()
+
+//3: Les patterns de features possibles et leurs nombre d’occurences ((“”, 25623541), (“T1, T32”,  52), ...)
+
+//RDD
+val RDD_ID_feature_value = RDD_feature_id_value.map(feature_id_values => (feature_id_values._2, (feature_id_values._1, feature_id_values._3)))
+
+val RDD_ID_patternFeature = RDD_ID_feature_value.combineByKey(initializeConcatenate, combinerConcatenate , mergeConcatenate)
+
+val RDD_PATTERN_id =  RDD_ID_patternFeature.map(id_pattern => (id_pattern._2, id_pattern._1))
+
+val RDD_PATTERN_count = RDD_PATTERN_id.combineByKey((id: String) => 1, (conteur: Int, id: String) => conteur + 1, (conteur1: Int, conteur2: Int) => conteur1 + conteur2)
+
+//dataFrame
+val DF_id_PatternFeature = DF_three_columns.groupBy("Id").agg(GroupConcat($"feature", $"value")).toDF(Seq("Id", "PatternFeature"): _*)
+
+val DF_countOccurenciPattern = DF_id_PatternFeature.groupBy("PatternFeature").count()
+
+//Vérifications:
+DF_countOccurenciPattern.groupBy().agg(sum("count"))
+
+val rdd_features = DF_countOccurenciPattern.rdd.map(roww => (roww(0).toString, roww(1)))
+val lf = rdd_features.leftOuterJoin(RDD_PATTERN_count)
+val rr = lf.filter(KEY_one_two => KEY_one_two._2._2 == None)
